1dc8e01e06256294039896a469337c5fa6d1f686
Sebastian Rentsch <rentsch_s@hotmail.com>
2023-05-15 13:00:54 +0200

    added picture import and remove

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,62 @@
-<script setup>
+<script>
 import { RouterLink, RouterView } from 'vue-router'
+import { ref } from 'vue';
 import './App.css'
+
+export default {
+  setup() {
+
+    const fileInput = ref(null)
+    let image = ref(null)
+    let showElement = ref(true)
+    let showButton = ref(false)
+
+    const triggerFileInput = () => {
+      fileInput.value.click()
+    }
+
+    const handleFileInput = (event) => {
+      const file = event.target.files[0]
+      if (file) {
+        const reader = new FileReader()
+        reader.onload = (e) => {
+          image.value = e.target.result
+        }
+        reader.readAsDataURL(file)
+        showElement.value = false
+      }
+    }
+
+    const removeImage = () => {
+      image.value = null
+      showElement.value = true
+      hideButtonHandler
+    }
+
+    const showButtonHandler = () => {
+      showButton.value = true
+    }
+
+    const hideButtonHandler = () => {
+      showButton.value = false
+    }
+
+
+    return {
+      RouterLink,
+      RouterView,
+      fileInput,
+      image,
+      showElement,
+      showButton,
+      triggerFileInput,
+      handleFileInput,
+      removeImage,
+      showButtonHandler,
+      hideButtonHandler
+    }
+  }
+}
 
 </script>
 
@@ -19,7 +75,12 @@ import './App.css'
       <RouterView></RouterView>
     </div>
     <div class="block-right">
-      <img src="" class="person-picture">
+      <input type="file" ref="fileInput" @change="handleFileInput" style="display: none;">
+      <button @click="triggerFileInput" v-if="showElement">Profilbild hinzufügen</button>
+      <div class="container-picture">
+        <img :src="image" @mouseover="showButtonHandler" @mouseleave="hideButtonHandler" v-if="image" class="person-picture">
+        <button @click="removeImage" @mouseover="showButtonHandler" class="button-remove" :class="{ 'visible': showButton }">X</button>
+      </div>
     </div>
   </div>
 </template>
